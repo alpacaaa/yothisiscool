a6152ca6c770d77e281c48b2a182f8f33c99630c
Andrea Buran <andreaburan@gmail.com>
2014-09-21 11:03:54 +0300

    Battle ground set up.

---
+++ config.rb
@@ -0,0 +1,21 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "styles/css"
+sass_dir = "styles/scss"
+images_dir = "media/images"
+javascripts_dir = "scripts/js"
+fonts_dir = "fonts"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+preferred_syntax = :scss
