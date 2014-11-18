6aaa1f9a4079e88526387684764c19b16eebc04a
Andrea Buran <andreaburan@gmail.com>
2014-11-18 22:54:16 +0200

    Form and various stuff improved. Playing with colors started.

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,1 +1,21 @@
+// @codekit-prepend "jquery-2.1.1.js"
 
+/*$('.button').on( 'click', 'input[type = "submit"]', function() {
+	alert("banana");
+});
+
+$('input').on( 'click', function() {
+	alert('banana');
+});
+
+$('.error input, .error textarea').focus(function() {
+	//alert('banana!');
+	$(this).parents('div').removeClass('error');
+});*/
+
+$('.action').on( 'click', 'input[type = "submit"]', function() {
+	$(this).parents('div').removeClass('closed');
+	$(this).parents('div').addClass('open');
+	//$(this).toggleClass( 'closed', addOrRemove );
+	return false;
+});
