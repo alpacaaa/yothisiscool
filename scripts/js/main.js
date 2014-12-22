9137663645c51bdd385bc87e3cd3dd323f8c35e0
Andrea Buran <andreaburan@gmail.com>
2014-12-22 19:01:07 +0100

    Packery implemented.

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,16 +1,66 @@
 // @codekit-prepend "jquery-2.1.1.js"
+// @codekit-prepend "imagesloaded.pkgd.js"
+// @codekit-prepend "packery.pkgd.min.js"
 
-/*$('.button').on( 'click', 'input[type = "submit"]', function() {
-	alert("banana");
+/*
+ * Comments
+ */
+
+// http://codepen.io/desandro/pen/chisC
+
+// overwrite Packery methods
+var __resetLayout = Packery.prototype._resetLayout;
+Packery.prototype._resetLayout = function() {
+	__resetLayout.call( this );
+	// reset packer
+	var parentSize = getSize( this.element.parentNode );
+	var colW = this.columnWidth + this.gutter;
+	this.fitWidth = Math.floor( ( parentSize.innerWidth + this.gutter ) / colW ) * colW;
+	console.log( colW, this.fitWidth )
+	this.packer.width = this.fitWidth;
+	this.packer.height = Number.POSITIVE_INFINITY;
+	this.packer.reset();
+};
+
+
+Packery.prototype._getContainerSize = function() {
+	// remove empty space from fit width
+	var emptyWidth = 0;
+	for ( var i=0, len = this.packer.spaces.length; i < len; i++ ) {
+		var space = this.packer.spaces[i];
+		if ( space.y === 0 && space.height === Number.POSITIVE_INFINITY ) {
+			emptyWidth += space.width;
+		}
+	}
+
+	return {
+		width: this.fitWidth - this.gutter - emptyWidth,
+		height: this.maxY - this.gutter
+	};
+};
+
+// always resize
+Packery.prototype.resize = function() {
+	this.layout();
+};
+
+
+docReady( function() {
+	var container = document.querySelector('.comments');
+	var pckry = new Packery( container, {
+		itemSelector: '.comment',
+		columnWidth: 288,
+		gutter: 0
+	});
 });
 
-$('input').on( 'click', function() {
-	alert('banana');
-});
-
-$('.error input, .error textarea').focus(function() {
-	//alert('banana!');
-	$(this).parents('div').removeClass('error');
+/*var $container = $('.comments').imagesLoaded( function() {
+	// initialize Packery after all images have loaded
+	$container.packery({
+		itemSelector: '.comment',
+		columnWidth: 288,
+		gutter: 0
+	});
 });*/
 
 /*
@@ -23,11 +73,10 @@ $('.error input, .error textarea').focus(function() {
 });
 
 $('.form-comment').on( 'click', 'input[type = "submit"]', function() {
-	$(this).parents('.form-comment').toggleClass('close');
+	$(this).parents('.form-comment').toggleClass('close').find('.field textarea').focus();
 	//$(this).removeClass('closed');
 	//.focus();
 	//alert("banana");
 	//$(this).toggleClass( 'closed', addOrRemove );
 	return false;
-
 });
