b04f987ccb7aece99e322bc773690453ed1ed382
Andrea Buran <ranbureand@users.noreply.github.com>
2015-03-02 22:41:16 +0200

    Tidy up logo Javascript.

--- client/app/components/dude-logo.js
+++ client/app/components/dude-logo.js
@@ -1,27 +1,20 @@
 
 import Ember from 'ember';
 
-
 function positionLogo() {
 
   var coin,
-  logoSize,
   posX, posY,
   rotation;
 
   coin = Math.floor(Math.random() * 2);
-  logoSize = 128;
 
   function getRandomInt(min, max) {
   	return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  //var posX = (Math.floor((Math.random() * ($(window).width() - logoSize))/12) * 12);
-  //var posY = (Math.floor((Math.random() * ($(window).height() - logoSize))/12) * 12);
-
   if (coin) {
   	posX = getRandomInt(5, 30);
-  	//posX = Math.floor((Math.random() * 100) + 1);
   } else {
   	posX = getRandomInt(75, 100);
   }
@@ -39,7 +32,6 @@ function positionLogo() {
   });
 
 }
-
 
 var DudeLogoComponent = Ember.Component.extend({
   tagName: '',
